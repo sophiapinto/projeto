560b75b70e676d9c87aa685974e1adb2f8bdbda7
Sophia Pinto <sarahsophiapinto@hotmail.com>
2023-03-18 12:32:02 -0300

    Atualização tela de login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import Header from '../../components/Header';
-import Footer from '../../components/Footer'
+import logo from "../../assets/images/logo.png";
+
+//import Header from '../../components/Header';
+//import Footer from '../../components/Footer'
 import { Link } from "react-router-dom";
 
 import "./home.css";
@@ -9,15 +11,32 @@ import "./home.css";
 function Home () {
   return (
     <div>
-      <Header />,
+
+        <div className= "logo">
+          <img src= {logo} alt="Senhora Vacina" />
+          <h1>CALENDÁRIO VACINAL</h1>
+        </div>
 
       <div className="acess">
         <Link to="/Login">
           <h2 className="text-acess">Acesse</h2>
         </Link>
-      </div>,
-      
-      <Footer />
+      </div>
+
+      <div className="footer-wrapper">
+        
+        <span className="footer-text">Plataforma desenvolvida por Elisa e Sophia Pinto.<br/>Copyright&copy;&nbsp;2022.<br/>Todos os direitos reservados.</span>
+        {/*<div className="footer-navbar">
+          <a href="/" className="footer-link">
+            Termos de uso
+          </a>
+          <a href="/" className="footer-link">
+            Política de privacidade
+          </a>
+        </div>
+        */}
+        
+      </div>
       
     </div>
   );
