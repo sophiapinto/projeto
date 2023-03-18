7b9d387579f29a394a65e73fb2285c32c1465ee9
Sophia Pinto <sarahsophiapinto@hotmail.com>
2023-03-18 16:16:41 -0300

    atualização da tela de aprazamento

--- src/pages/Aprazamento/index.js
+++ src/pages/Aprazamento/index.js
@@ -1,13 +1,9 @@
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 import PrazoVacinas from '../../components/PrazoVacinas';
 
 function Aprazamento () {
   return (
     <div>
-      <Header />
       <PrazoVacinas />
-      <Footer  />
     </div>
   );
 }
