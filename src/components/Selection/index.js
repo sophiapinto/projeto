1f5776ba2226996dfed613b5987075e50dd640a1
Sophia Pinto <sarahsophiapinto@hotmail.com>
2023-03-18 15:32:44 -0300

    Atualização tela de seleção

--- src/components/Selection/index.js
+++ src/components/Selection/index.js
@@ -5,8 +5,9 @@ import calendario from "../../assets/images/calendario.svg";
 
 import { Link } from "react-router-dom";
 
-import gestante from "../../assets/images/gestante.svg";
+import logo from "../../assets/images/logo.png";
 
+import gestante from "../../assets/images/gestante.svg";
 import idoso from "../../assets/images/idoso.svg";
 import adulto from "../../assets/images/adulto.svg";
 import adolescente from "../../assets/images/adolescente.svg";
@@ -17,59 +18,61 @@ class Selection extends PureComponent {
     render() {
       return (
         <Fragment>
+        <div className= "logo">
+          <img src= {logo} alt="Senhora Vacina" />
+        </div>
+        <span className="text-span">CALENDÁRIO VACINAL</span>
 
-                <div className="calendario-wrapper">
-                    <figure class="figure-calendario">
-                        <img src={calendario} alt="Calendario"/>
-                    </figure>
-                </div>
+            <div className="calendario-wrapper">
+               <figure class="figure-calendario">
+                    <img src={calendario} alt="Calendario"/>
+               </figure>
+            </div>
 
             <div className="selection-wrapper">
 
-            <section className="grid-container">
-                <div className= "grid-item">
+                <div className= "grid-item-01">
                     <Link to="/Aprazamento">
                         <img src= {gestante} alt="Gestante" />
-                        <span className="text-selection">GESTANTE</span>
+                        <span className="text-selection-01">GESTANTE</span>
                     </Link>
                 </div>
 
-                <div className= "grid-item">
+                <div className= "grid-item-02">
                     <Link to="/Aprazamento">
                         <img src= {idoso} alt="Idoso" />
-                        <span className="text-selection">IDOSO</span>
+                        <span className="text-selection-02">IDOSO</span>
                     </Link>
                 </div>
 
-                <div className= "grid-item">
+                <div className= "grid-item-03">
                     <Link to="/Aprazamento">
                         <img src= {adulto} alt="Adulto" />
-                        <span className="text-selection">ADULTO</span>
+                        <span className="text-selection-03">ADULTO</span>
                     </Link>
                 </div>
 
-                <div className= "grid-item">
+                <div className= "grid-item-04">
                     <Link to="/Aprazamento">
                         <img src= {adolescente} alt="Adolescente" />
-                        <span className="text-selection">ADOLESCENTE</span>
+                        <span className="text-selection-04">ADOLESCENTE</span>
                     </Link>
                 </div>
 
-                <div className= "grid-item">
+                <div className= "grid-item-05">
                     <Link to="/Aprazamento">
                         <img src= {criança} alt="Criança" />
-                        <span className="text-selection">CRIANÇA</span>
+                        <span className="text-selection-05">CRIANÇA</span>
                     </Link>
                 </div>
                 
-                <div className= "grid-item">
+                <div className= "grid-item-06">
                     <Link to="/Aprazamento">
                         <img src= {aprazamento} alt="Aprazamento" />
-                        <span className="text-selection">APRAZAMENTO</span>
+                        <span className="text-selection-06">APRAZAMENTO</span>
                     </Link>
                 </div>
 
-            </section>
             </div>
 
         
--- src/pages/Selecao/index.js
+++ src/pages/Selecao/index.js
@@ -1,14 +1,9 @@
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 import Selection from '../../components/Selection';
-import "./selecao.css";
 
 function Selecao () {
   return (
     <div>
-      <Header />
       <Selection />
-      <Footer  />
     </div>
   );
 }
