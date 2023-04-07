a3f608fc4271746bd0e24d63a01ec6e94274c4de
Sophia Pinto <sarahsophiapinto@hotmail.com>
2023-04-07 19:53:33 -0300

    Atualização letra gestante

--- src/pages/VacinaGestante/index.js
+++ src/pages/VacinaGestante/index.js
@@ -15,57 +15,57 @@ function VacinaGestante () {
         </div>
         <span className="text-gest-calenderio">CALENDÁRIO VACINAL DA GESTANTE</span>
 
-        <div className= "wrapper-vacina-item">
+        <div className= "wrapper-vacina-item-gestante">
             <div className="logo-vacina">
                 <img src= {vacinalogo} alt="logo" />
             </div>
-            <span className="text-nome-vacina">Difteria e Tétano (dT)</span>
-            <span className="text-quant-doses">Número de doses:</span>
-            <span className="text-infomacao">Informações:</span>
+            <span className="text-nome-vacina-gestante">Difteria e Tétano (dT)</span>
+            <span className="text-quant-doses-gestante">Número de doses:</span>
+            <span className="text-infomacao-gestante">Informações:</span>
             
-            <div className="wrapper-quant-doses">
+            <div className="wrapper-quant-doses-gestante">
                 <span className="text-quant-doses-02">3</span>
             </div>
 
             
-            <div className="wrapper-informacoes">
-            <span className="text-informacao-2">Iniciar ou completar o esquema, de acordo com a situação vacinal.</span>
+            <div className="wrapper-informacoes-gestante">
+            <span className="text-informacao-2-gestante">Iniciar ou completar o esquema, de acordo com a situação vacinal.</span>
             </div>
         </div>
 
-        <div className= "wrapper-vacina-item-02">
+        <div className= "wrapper-vacina-item-02-gestante">
             <div className="logo-vacina">
                 <img src= {vacinalogo} alt="logo" />
             </div>
-            <span className="text-nome-vacina">Hepatite B recombinante</span>
-            <span className="text-quant-doses">Número de doses:</span>
-            <span className="text-infomacao">Informações:</span>
+            <span className="text-nome-vacina-gestante">Hepatite B recombinante</span>
+            <span className="text-quant-doses-gestante">Número de doses:</span>
+            <span className="text-infomacao-gestante">Informações:</span>
             
-            <div className="wrapper-quant-doses">
-                <span className="text-quant-doses-02">3</span>
+            <div className="wrapper-quant-doses-gestante">
+                <span className="text-quant-doses-02-gestante">3</span>
             </div>
 
             
-            <div className="wrapper-informacoes">
-                <span className="text-informacao-2">Iniciar ou completar o esquema, de acordo com a situação vacinal.</span>
+            <div className="wrapper-informacoes-gestante">
+                <span className="text-informacao-2-gestante">Iniciar ou completar o esquema, de acordo com a situação vacinal.</span>
             </div>
         </div>
 
-        <div className= "wrapper-vacina-item-03">
+        <div className= "wrapper-vacina-item-03-gestante">
             <div className="logo-vacina">
                 <img src= {vacinalogo} alt="logo" />
             </div>
-            <span className="text-nome-vacina">Difteria, Tétano, Pertussis acelular</span>
-            <span className="text-quant-doses">Número de doses:</span>
-            <span className="text-infomacao">Informações:</span>
+            <span className="text-nome-vacina-gestante">Difteria, Tétano, Pertussis acelular</span>
+            <span className="text-quant-doses-gestante">Número de doses:</span>
+            <span className="text-infomacao-gestante">Informações:</span>
             
-            <div className="wrapper-quant-doses">
-                <span className="text-quant-doses-02">1</span>
+            <div className="wrapper-quant-doses-gestante">
+                <span className="text-quant-doses-02-gestante">1</span>
             </div>
 
             
-            <div className="wrapper-informacoes">
-                <span className="text-informacao-2">1 dose para gestante a partir da 20º semana de gravidez, 1 dose a cada gestação.</span>
+            <div className="wrapper-informacoes-gestante">
+                <span className="text-informacao-2-gestante">1 dose para gestante a partir da 20º semana de gravidez, 1 dose a cada gestação.</span>
             </div>
         </div>            
     </div>
