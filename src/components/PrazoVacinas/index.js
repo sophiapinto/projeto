922e6d3ae4e890900d948dffa87e923564ba1d96
Sophia Pinto <sarahsophiapinto@hotmail.com>
2023-04-29 12:52:56 -0300

    Calculo vac tentativa

--- src/components/PrazoVacinas/index.js
+++ src/components/PrazoVacinas/index.js
@@ -1,104 +1,60 @@
 import React, { PureComponent } from "react";
-//import { Link } from "react-router-dom";
 
 import dados from "../../vacinas.json";
 import dadosVAC from "../../dados.json";
 
-import logo from "../../assets/images/logo.png";
-
-//import { Link } from "react-router-dom";
-//import calendario from "../../assets/images/calendario.svg";
-//import pesquisa from "../../assets/images/logo.png";
-//import aprazamento from "../../assets/images/aprazamento.svg";
+//import logo from "../../assets/images/logo.png";
 
 import './prazovacinas.css';
 class PrazoVacinas extends PureComponent {
-    render() {
-      return (
-        <div>
-          <div className= "logo">
-            <img src= {logo} alt="Senhora Vacina" />
-          </div>
-          <span className="text-span">APRAZAMENTO DE VACINAS</span>
-          
-          <div className="wrapper-pesquisa">
-            <form className="Form-Prazo-Vacinas">  
-            <div className="selection-faixaetaria">
-              <select class="pesquisa-faixa-etaria" name="option">
-                <option value="" disabled selected>Selecione a faixa etária:</option>
-                {
-                  dados.categorias.map((v,i)=>
-                  <option value={i}>
-                    {v.titulo}
-                  </option>
-                )}
-              </select>
-            </div>
 
-            <div className="selection-vacina">
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
-                <select class="pesquisa-vacina" name="option">
-                <option value="" disabled selected>Selecione a vacina:</option>
-                {
-                  dadosVAC.map((v,i)=>
-                  <option value={i}>
-                    {v.vacina}
-                  </option>
-                )}
-              </select>         
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-            </div>
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
-            <div className="data">
-                <input type="date" className="pesquisa-data" placeholder="Insira a data:" />
-            </div>
+  handleSubmit(event) {
+    alert('A data da próxima vacina é: ' + this.state.value);
+    event.preventDefault();
+  }
 
-            {/*
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <select type="checkbox" value={this.state.value} onChange={this.handleChange}>
+                  <option value="" disabled selected>Selecione a faixa etária:</option>
+                  {
+                    dados.categorias.map((v,i)=> <option value={i}> {v.titulo} </option>
+                  )}
+        </select>
+        </label>
 
-                        <div className="selection-faixaetaria">
-              <select class="pesquisa-faixa-etaria" name="option">
-                <option value="" disabled selected>Selecione a faixa etária:</option>
-                <option value="1">Recém-nascido</option>
-                <option value="2">Criança</option>
-                <option value="3">Adolescente</option>
-                <option value="4">Adulto</option>
-                <option value="5">Idoso</option>
-                <option value="6">Gestante</option>
-              </select>
-            </div>
+        <label>
+          <select type="checkbox" value={this.state.value} onChange={this.handleChange}>
+            <option value="" disabled selected>Selecione a vacina:</option>
+            {
+              dadosVAC.map((v,i)=> <option value={i}> {v.vacina} </option>
+            )}
+          </select>
+        </label>
 
-                  function ListaVacinas ((vacinas)){
-                    dados.map((titulo)=> <ListaVacinas vacinas= {titulo.vacinas}/>)
-                  }
-                  return ListaVacinas ();
+        <label>
+        <input type="date"/>
+        </label>
 
-            <div className="selection-vacina">
-              <select class="pesquisa-vacina" name="option">
-                <option value="" disabled selected>Selecione a vacina:</option>
-                <option value="1">BCG</option>
-                <option value="2">Hepatite</option>
-              </select>
-            </div>
 
-            <div className="data">
-              <input type="date" className="pesquisa-data" placeholder="Insira a data:" />
-            </div>
-
-            <Link to= "/">
-            <div className="pesquisa-button">
-              <span className="text-pesquisa-button">Pesquisar</span>
-            </div>
-            </Link>
-
-            */}
-
-            <input type="submit" class="pesquisa-button" onclick="Calcular();" value="Pesquisar" />
-            </form>
-
-          </div>
-        </div>  
-      );
-    }
+        <input type="submit" value="Enviar" />
+      </form>
+    );
+  }
 }
 
 export default PrazoVacinas;
