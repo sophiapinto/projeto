f33ded4a309b2ea3cf57b7fe5c04c69146f5717e
Sophia Pinto <sarahsophiapinto@hotmail.com>
2023-06-18 18:21:21 -0300

    Ajuste de css tela de seleção

--- src/components/Selection/index.js
+++ src/components/Selection/index.js
@@ -5,7 +5,7 @@ import calendario from "../../assets/images/calendario.svg";
 
 import { Link } from "react-router-dom";
 
-import logo from "../../assets/images/logo.png";
+//import logo from "../../assets/images/logo.png";
 
 import gestante from "../../assets/images/gestante.svg";
 import idoso from "../../assets/images/idoso.svg";
@@ -18,64 +18,67 @@ class Selection extends PureComponent {
     render() {
       return (
         <Fragment>
+
+            {/*
         <div className= "logo-18">
           <img src= {logo} alt="Senhora Vacina" />
         </div>
         <span className="text-span-18">CALENDÁRIO VACINAL</span>
+    */}
 
             <div className="calendario-wrapper">
                <figure className="figure-calendario">
                     <img src={calendario} alt="Calendario"/>
                </figure>
             </div>
+            <div class="container">
+                <div className="selection-wrapper">
 
-            <div className="selection-wrapper">
+                    <figure class= "select-img grid-item-01">
+                        <Link to="/VacinaGestante">
+                            <img src= {gestante} alt="Gestante" />
+                            <span className="text-selection">GESTANTE</span>
+                        </Link>
+                    </figure>
 
-                <div className= "grid-item-01">
-                    <Link to="/VacinaGestante">
-                        <img src= {gestante} alt="Gestante" />
-                        <span className="text-selection-01">GESTANTE</span>
-                    </Link>
-                </div>
+                    <figure class= "select-img grid-item-02">
+                        <Link to="/VacinaIdoso">
+                            <img src= {idoso} alt="Idoso" />
+                            <span className="text-selection">IDOSO</span>
+                        </Link>
+                    </figure>
 
-                <div className= "grid-item-02">
-                    <Link to="/VacinaIdoso">
-                        <img src= {idoso} alt="Idoso" />
-                        <span className="text-selection-02">IDOSO</span>
-                    </Link>
-                </div>
+                    <figure class= "select-img grid-item-03">
+                        <Link to="/VacinaAdulto">
+                            <img src= {adulto} alt="Adulto" />
+                            <span className="text-selection">ADULTO</span>
+                        </Link>
+                    </figure>
 
-                <div className= "grid-item-03">
-                    <Link to="/VacinaAdulto">
-                        <img src= {adulto} alt="Adulto" />
-                        <span className="text-selection-03">ADULTO</span>
-                    </Link>
-                </div>
+                    <figure class= "select-img grid-item-04">
+                        <Link to="/VacinaAdolescente">
+                            <img src= {adolescente} alt="Adolescente" />
+                            <span className="text-selection">ADOLESCENTE</span>
+                        </Link>
+                    </figure>
 
-                <div className= "grid-item-04">
-                    <Link to="/VacinaAdolescente">
-                        <img src= {adolescente} alt="Adolescente" />
-                        <span className="text-selection-04">ADOLESCENTE</span>
-                    </Link>
-                </div>
+                    <figure class= "select-img grid-item-05">
+                        <Link to="/VacinaCrianca">
+                            <img src= {criança} alt="Criança" />
+                            <span className="text-selection">CRIANÇA</span>
+                        </Link>
+                    </figure>
+                    
+                    <figure class= "select-img grid-item-06">
+                        <Link to="/Aprazamento">
+                            <img src= {aprazamento} alt="Aprazamento" />
+                            <span className="text-selection">APRAZAMENTO</span>
+                        </Link>
+                    </figure>
 
-                <div className= "grid-item-05">
-                    <Link to="/VacinaCrianca">
-                        <img src= {criança} alt="Criança" />
-                        <span className="text-selection-05">CRIANÇA</span>
-                    </Link>
-                </div>
-                
-                <div className= "grid-item-06">
-                    <Link to="/Aprazamento">
-                        <img src= {aprazamento} alt="Aprazamento" />
-                        <span className="text-selection-06">APRAZAMENTO</span>
-                    </Link>
                 </div>
 
             </div>
-
-        
         </Fragment>
   
   
--- src/pages/Selecao/index.js
+++ src/pages/Selecao/index.js
@@ -1,9 +1,16 @@
 import Selection from '../../components/Selection';
+import Header from '../../components/Header';
+//import Footer from '../../components/Footer';
+
+import './selecao.css';
 
 function Selecao () {
   return (
     <div>
+      <Header  />
       <Selection />
+      {/*<Footer />*/}
+
     </div>
   );
 }
